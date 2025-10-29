3e85d67ec785ad421375348fc78c1ddcce9e1b60
WeitongSun <47732803+WeitongSun@users.noreply.github.com>
2025-10-29 11:20:32 -0400

    Initial commit

---
+++ sketch_oct29a/sketch_oct29a.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_CircuitPlayground.h>
+//Code for touch senser on CPX
+//Made by Weitong
+int value;
+int freq;
+
+void setup() {
+  CircuitPlayground.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  value = analogRead(A3);
+  //delay(1000);
+  Serial.println(value);
+  
+  freq=map(value,0,1023,100,1000);
+  //CircuitPlayground.playTone(freq,1000);
+}
